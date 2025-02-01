47af4ec640e55082a1f8392d99a1693f65abcac1
Patrick <patrsc@nubu.at>
2025-02-01 15:00:30 +0100

    Add title and favicon

--- web/app.vue
+++ web/app.vue
@@ -1,3 +1,15 @@
 <template>
-  <Map />
+    <Map />
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: 'MapLineDraw',
+    meta: [
+      { name: 'description', content: 'Sketch corridors of railway lines or roads on an interactive map.' },
+    ],
+    link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: 'any' }
+    ],
+})
+</script>
