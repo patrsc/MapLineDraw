71f722969394a608c73dc235f96657e1baec8848
Patrick <patrsc@nubu.at>
2025-02-08 20:44:54 +0100

    Add MapView.vue

---
+++ web/components/MapView.vue
@@ -0,0 +1,17 @@
+<template>
+    MapView
+</template>
+
+<script setup>
+// MapView component
+// model:
+// - project.curves
+// - project.map
+// - selectedCurveIndex
+// props:
+// - colorMap
+// state:
+// - curvesCache
+// events:
+// - @select-curve: f(properties)
+</script>
