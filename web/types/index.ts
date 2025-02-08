c9312acf29e3f1c8905af260bf727a52758677fa
Patrick <patrsc@nubu.at>
2025-02-08 22:01:26 +0100

    Define types

--- web/types/index.ts
+++ web/types/index.ts
@@ -1,4 +1,9 @@
 // Types
+import { CircleMarker, Polyline } from "leaflet"
+
+// Common
+export type int = number
+export type float = number
 
 // Color map related
 export type ColorMapLimit = number | null
@@ -15,6 +20,11 @@ export interface ColorMap {
     name: string
     items: ColorMapItem[]
 }
+export interface PreparedColorMap {
+    colors: ColorMapColor[]
+    limits: number[]
+    factors: number[]
+}
 
 // Coordinates
 export interface CartesianPoint {
@@ -25,3 +35,56 @@ export interface GlobePoint {
     lat: number
     lon: number
 }
+
+// Project
+export interface ProjectInfo {
+    name: string
+    description: string
+    author: string
+}
+export interface Curve {
+    name: string
+    controlPoints: GlobePoint[]
+    closed: boolean
+}
+export type MapBackground = string
+export interface MapSettings {
+    center: GlobePoint
+    zoom: number
+    background: MapBackground,
+}
+export interface Settings {
+    selectedColorMapIndex: number
+    map: MapSettings
+}
+
+export interface Project {
+    info: ProjectInfo
+    curves: Curve[]
+    colorMaps: ColorMap[]
+    settings: Settings
+}
+
+
+// Cache
+export interface SplineData {
+    degree: int
+    lat: float[]
+    lon: float[]
+    distance: float[]
+    curvature: float[]
+    speed: float[]
+}
+export interface Spline {
+    requestedId: number
+    id: number
+    data: null | SplineData
+}
+export type LayerItem = CircleMarker | Polyline
+export interface CurveCacheItem {
+    points: CircleMarker[]
+    spline: Spline
+    layers: LayerItem[]
+}
+
+export type CurveCache = CurveCacheItem[]
