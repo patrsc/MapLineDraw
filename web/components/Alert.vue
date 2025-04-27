f71bf3dd874daa70277c1d335a717e815db19d6b
Patrick <patrsc@nubu.at>
2025-04-27 21:21:43 +0200

    Add Alert and Modal components

---
+++ web/components/Alert.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="classes">
+        <div>
+            <Ico :name="icon" class="me-2" />
+        </div>
+        <div class="flex-grow-1 pre-wrap">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    type: {type: String, default: "danger"}
+})
+const icon = computed(() => {
+    switch (props.type) {
+        case "danger": case "warning":
+            return "fa6-solid:triangle-exclamation"
+        case "success":
+            return "fa6-solid:circle-check"
+        default:
+            return "fa6-solid:circle-info"
+    }
+})
+const classes = computed(() => ["alert", `alert-${props.type}`, "mt-3", "d-flex", "flex-row"])
+</script>
---
+++ web/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="modal fade" ref="modalElement" :id="props.id" tabindex="-1"
+        :aria-labelledby="labelId" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" :id="labelId">
+                    <slot name="title"></slot>
+                </h1>
+                <button type="button" class="btn-close"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <slot></slot>
+            </div>
+            <div v-if="!nofooter" class="modal-footer">
+                <button type="button" class="btn btn-secondary"
+                    data-bs-dismiss="modal">Cancel</button>
+                <slot name="footer"></slot>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    id: string,
+    nofooter?: boolean,
+}
+const open = defineModel<boolean>()
+const emits = defineEmits(['hide', 'shown'])
+const props = defineProps<Props>()
+const labelId = computed(() => props.id + "Label")
+
+import type { Modal } from 'bootstrap'
+
+const { $bs } = useNuxtApp()
+
+const element = useTemplateRef('modalElement')
+const modal = ref(undefined as undefined | Modal)
+
+function initModal() {
+    if (!modal.value && element.value) {
+        modal.value = new $bs.Modal(element.value as Element)
+        element.value.addEventListener('hide.bs.modal', event => {
+            emits('hide')
+            open.value = false
+        })
+        element.value.addEventListener('shown.bs.modal', event => {
+            emits('shown')
+        })
+    }
+}
+
+watch(element, initModal, { immediate: true })
+watch(open, (to, from) => {
+    if (modal.value) {
+        if (to && !from) {
+            modal.value.show()
+        }
+        if (!to && from) {
+            modal.value.hide()
+        }
+    }
+})
+
+</script>
