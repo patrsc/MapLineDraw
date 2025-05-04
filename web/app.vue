78dab377020e87c56b7083aa307bae9ca2b45380
Patrick <patrsc@nubu.at>
2025-05-04 15:03:35 +0200

    Add SEO

--- web/app.vue
+++ web/app.vue
@@ -7,9 +7,13 @@ useHead({
     title: 'MapLineDraw',
     meta: [
       { name: 'description', content: 'Sketch corridors of railway lines or roads on an interactive map.' },
+      { property: 'og:title', content: 'MapLineDraw'},
+      { property: 'og:description', content: 'Sketch corridors of railway lines or roads on an interactive map.'},
+      { property: 'og:url', content: 'https://maplinedraw.com/'},
     ],
     link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: 'any' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: 'any' },
+        { rel: 'canonical', href: 'https://maplinedraw.com/'}
     ],
 })
 </script>
