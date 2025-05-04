2c4ed267dd0f8d1add0729b655dcd4a40f057d38
Patrick <patrsc@nubu.at>
2025-05-04 21:13:14 +0200

    Prepare API

--- api/api.py
+++ api/api.py
@@ -4,7 +4,8 @@ from typing import Annotated
 from annotated_types import Len
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, ConfigDict, StringConstraints
+from pydantic_string_url import HttpUrl
 import numpy as np
 
 from globe import GlobePoint, Point
@@ -14,7 +15,10 @@ from geo import arclen, curvature, speed
 API_ROOT_PATH = os.environ.get("API_ROOT_PATH", "/")
 API_ALLOWED_ORIGIN = os.environ.get("API_ALLOWED_ORIGIN", "http://localhost:3000")
 
-app = FastAPI(title="Curve API", root_path=API_ROOT_PATH)
+MAX_FILE_SIZE = 1 * 1024 * 1024
+MAX_FILE_URL_LENGTH = 250
+
+app = FastAPI(title="MapLineDraw API", root_path=API_ROOT_PATH)
 
 origins = [
     API_ALLOWED_ORIGIN,
@@ -65,6 +69,86 @@ class CurveOutput(BaseModel):
     speed: list[float]
 
 
+class PublishInput(BaseModel):
+    """Publish inputs."""
+    url: HttpUrl
+
+
+class PublishOutput(BaseModel):
+    """Publish outputs."""
+    id: str
+
+
+class ProjectInfo(BaseModel):
+    """Project info."""
+    name: str
+    description: str
+    author: str
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class LatLonPoint(BaseModel):
+    """Control point."""
+    lat: float = Field(ge=-90, le=90)
+    lon: float = Field(ge=-180, le=180)
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class ProjectCurve(BaseModel):
+    """Curve."""
+    name: str
+    controlPoints: list[LatLonPoint]
+    closed: bool
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class ProjectColorMapItem(BaseModel):
+    """Color map item."""
+    limit: None | float
+    color: Annotated[str, StringConstraints(pattern=r'^#[0-9a-fA-F]{6}$')]
+    label: str
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class ProjectColorMap(BaseModel):
+    """Color map."""
+    name: str
+    items: list[ProjectColorMapItem]
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class ProjectMapSettings(BaseModel):
+    """Project settings."""
+    center: LatLonPoint
+    zoom: int = Field(ge=0)
+    background: str
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class ProjectSettings(BaseModel):
+    """Project settings."""
+    selectedColorMapIndex: int = Field(ge=0)
+    map: ProjectMapSettings
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class Project(BaseModel):
+    """Project."""
+    info: ProjectInfo
+    curves: list[ProjectCurve]
+    colorMaps: list[ProjectColorMap]
+    settings: ProjectSettings
+
+    model_config = ConfigDict(extra='forbid')
+
+
 @app.post("/curve", response_model=CurveOutput)
 def compute_curve(data: CurveInput) -> CurveOutput:
     """Compute B-spline curve."""
@@ -97,3 +181,43 @@ def compute_curve(data: CurveInput) -> CurveOutput:
         curvature=c,
         speed=v,
     )
+
+
+@app.post("/publish", response_model=PublishOutput)
+def publish_project(data: PublishInput) -> PublishOutput:
+    """Publish a project."""
+    # Verify URL length limit or fail with 400
+
+    # Download file from URL or fail with 404 if source file cannot be accessed
+
+    # Verify file size limit or fail with 422
+
+    # Parse JSON or fail with 422
+
+    # Validate JSON schema or fail with 422
+
+    # Generate public URL
+
+    # Store item including current date
+
+    print(data)
+    raise NotImplementedError()
+
+
+@app.get("/public/:id", response_model=Project)
+def get_public_project(id: str) -> Project:
+    """Get a published project as JSON."""
+    # Get stored URL or fail with 404
+
+    # Download file from URL or fail with 404 if source file cannot be accessed
+
+    # Verify file size limit or fail with 422
+
+    # Parse JSON or fail with 422
+
+    # Validate JSON schema or fail with 422
+
+    # Return project
+
+    print(id)
+    raise NotImplementedError()
